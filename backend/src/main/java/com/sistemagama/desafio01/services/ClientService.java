eb9747b55cc4226a3af419acd90166555105aa1f
Matheus M. Jeronimo <matheus.martins.jeronimo@gmail.com>
2021-09-23 13:48:00 -0300

    Client by id without exception handling

--- backend/src/main/java/com/sistemagama/desafio01/services/ClientService.java
+++ backend/src/main/java/com/sistemagama/desafio01/services/ClientService.java
@@ -1,6 +1,7 @@
 package com.sistemagama.desafio01.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,5 +23,11 @@ public class ClientService {
 		List<Client> list = repository.findAll();
 		List<ClientDTO> listDto = list.stream().map(x -> new ClientDTO(x)).collect(Collectors.toList());
 		return listDto;
+	}
+
+	public ClientDTO findById(Long id) {
+		Optional<Client> obj = repository.findById(id);
+		Client entity = obj.get();
+		return new ClientDTO(entity);
 	}
 }
